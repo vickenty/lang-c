fb122d4103f387f2d6f1b11c6479b2e2b0247a07
Vickenty Fesunov <kent@setattr.net>
2017-10-23 23:29:19 +0200

    Add meminfo program

---
+++ src/bin/meminfo.rs
@@ -0,0 +1,60 @@
+extern crate lang_c;
+use std::mem::size_of;
+use lang_c::ast::*;
+
+macro_rules! ps {
+    ($( $i:ident )+) => ($(println!("{:3}  {}", size_of::<$i>(), stringify!($i));)+)
+}
+
+fn main() {
+    ps! {
+        TypeOf
+        Identifier
+        Constant
+        Integer
+        Float
+        Expression
+        MemberOperator
+        GenericAssociation
+        UnaryOperator
+        BinaryOperator
+        OffsetDesignator
+        OffsetMember
+        Declaration
+        DeclarationSpecifier
+        InitDeclarator
+        StorageClassSpecifier
+        TypeSpecifier
+        StructType
+        StructDeclaration
+        SpecifierQualifier
+        StructDeclarator
+        Enumerator
+        TypeQualifier
+        FunctionSpecifier
+        AlignmentSpecifier
+        Declarator
+        DeclaratorKind
+        DerivedDeclarator
+        PointerQualifier
+        ArraySize
+        ParameterDeclaration
+        Ellipsis
+        TypeName
+        Initializer
+        InitializerListItem
+        Designator
+        StaticAssert
+        Statement
+        Label
+        ForInitializer
+        BlockItem
+        TranslationUnit
+        ExternalDeclaration
+        FunctionDefinition
+        Extension
+        AsmStatement
+        GnuAsmOperand
+        TypeOf
+    }
+}
